5d07d45318e2dfeeeb5d677a10115fe876aead03
sammed-21 <sammed2110@gmail.com>
2022-11-14 11:46:13 +0530

    dex's

--- packages/hardhat/contracts/DEX.sol
+++ packages/hardhat/contracts/DEX.sol
@@ -17,13 +17,15 @@ contract DEX {
 
     using SafeMath for uint256; //outlines use of SafeMath for uint256 variables
     IERC20 token; //instantiates the imported contract
+uint256 public totalLiquidity;  
+mapping(address => uint256) public liquidity;
 
     /* ========== EVENTS ========== */
 
     /**
      * @notice Emitted when ethToToken() swap transacted
      */
-    event EthToTokenSwap();
+    // event EthToTokenSwap(address indexed sender, uint256 exchanged );
 
     /**
      * @notice Emitted when tokenToEth() swap transacted
@@ -54,46 +56,79 @@ contract DEX {
      * @return totalLiquidity is the number of LPTs minting as a result of deposits made to DEX contract
      * NOTE: since ratio is 1:1, this is fine to initialize the totalLiquidity (wrt to balloons) as equal to eth balance of contract.
      */
-    function init(uint256 tokens) public payable returns (uint256) {}
+    function init(uint256 tokens) public payable returns (uint256) {
+        require(totalLiquidity == 0 ,"dex already have liquidity");
+        totalLiquidity = address(this).balance;
+        liquidity[msg.sender] = totalLiquidity;
+        require(token.transferFrom(msg.sender, address(this), tokens));
+        return totalLiquidity;
+    }
 
     /**
      * @notice returns yOutput, or yDelta for xInput (or xDelta)
      * @dev Follow along with the [original tutorial](https://medium.com/@austin_48503/%EF%B8%8F-minimum-viable-exchange-d84f30bd0c90) Price section for an understanding of the DEX's pricing model and for a price function to add to your contract. You may need to update the Solidity syntax (e.g. use + instead of .add, * instead of .mul, etc). Deploy when you are done.
      */
-    function price(
-        uint256 xInput,
-        uint256 xReserves,
-        uint256 yReserves
-    ) public view returns (uint256 yOutput) {}
+    // function price(
+    //     uint256 xInput,
+    //     uint256 xReserves,
+    //     uint256 yReserves
+    // ) public view returns (uint256 yOutput) {}
+     function price(uint256 input_amount, uint256 input_reserve, uint256 output_reserve) public view returns (uint256) {
+    uint256 input_amount_with_fee = input_amount * 997;
+    uint256 numerator = input_amount_with_fee * output_reserve;
+    uint256 denominator = input_reserve * 1000 + input_amount_with_fee;
+    return numerator / denominator;
+  }
 
-    /**
-     * @notice returns liquidity for a user. Note this is not needed typically due to the `liquidity()` mapping variable being public and having a getter as a result. This is left though as it is used within the front end code (App.jsx).
-     * if you are using a mapping liquidity, then you can use `return liquidity[lp]` to get the liquidity for a user.
-     *
-     */
-    function getLiquidity(address lp) public view returns (uint256) {}
-
+   
     /**
      * @notice sends Ether to DEX in exchange for $BAL
      */
-    function ethToToken() public payable returns (uint256 tokenOutput) {}
+ function ethToToken() public payable returns (uint256) {
+    uint256 token_reserve = token.balanceOf(address(this));
+    uint256 tokens_bought = price(msg.value, address(this).balance - msg.value, token_reserve);
+    require(token.transfer(msg.sender, tokens_bought));
+    // emit EthToTokenSwap(msg.sender,token_bought);
+    return tokens_bought;
+  }
 
-    /**
-     * @notice sends $BAL tokens to DEX in exchange for Ether
-     */
-    function tokenToEth(uint256 tokenInput) public returns (uint256 ethOutput) {}
-
+  function tokenToEth(uint256 tokens) public returns (uint256) {
+    uint256 token_reserve = token.balanceOf(address(this));
+    uint256 eth_bought = price(tokens, token_reserve, address(this).balance);
+    (bool sent, ) = msg.sender.call{value: eth_bought}("");
+    require(sent, "Failed to send user eth.");
+    require(token.transferFrom(msg.sender, address(this), tokens));
+    return eth_bought;
+  }
     /**
      * @notice allows deposits of $BAL and $ETH to liquidity pool
      * NOTE: parameter is the msg.value sent with this function call. That amount is used to determine the amount of $BAL needed as well and taken from the depositor.
      * NOTE: user has to make sure to give DEX approval to spend their tokens on their behalf by calling approve function prior to this function call.
      * NOTE: Equal parts of both assets will be removed from the user's wallet with respect to the price outlined by the AMM.
      */
-    function deposit() public payable returns (uint256 tokensDeposited) {}
+  function deposit() public payable returns (uint256) {
+    uint256 eth_reserve = address(this).balance - msg.value;
+    uint256 token_reserve = token.balanceOf(address(this));
+    uint256 token_amount = ((msg.value * token_reserve) / eth_reserve) + 1;
+    uint256 liquidity_minted = (msg.value * totalLiquidity) / eth_reserve;
+    liquidity[msg.sender] += liquidity_minted;
+    totalLiquidity += liquidity_minted;
+    require(token.transferFrom(msg.sender, address(this), token_amount));
+    return liquidity_minted;
+  }
 
     /**
      * @notice allows withdrawal of $BAL and $ETH from liquidity pool
      * NOTE: with this current code, the msg caller could end up getting very little back if the liquidity is super low in the pool. I guess they could see that with the UI.
      */
-    function withdraw(uint256 amount) public returns (uint256 eth_amount, uint256 token_amount) {}
-}
+ function withdraw(uint256 liq_amount) public returns (uint256, uint256) {
+    uint256 token_reserve = token.balanceOf(address(this));
+    uint256 eth_amount = (liq_amount * address(this).balance) / totalLiquidity;
+    uint256 token_amount = (liq_amount * token_reserve) / totalLiquidity;
+    liquidity[msg.sender] -= liq_amount;
+    totalLiquidity -= liq_amount;
+    (bool sent, ) = msg.sender.call{value: eth_amount}("");
+    require(sent, "Failed to send user eth.");
+    require(token.transfer(msg.sender, token_amount));
+    return (eth_amount, token_amount);
+  }}
--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -1,8 +1,8 @@
-// deploy/00_deploy_your_contract.js
+// // deploy/00_deploy_your_contract.js
 
-const { ethers } = require("hardhat");
+// const { ethers } = require("hardhat");
 
-const localChainId = "31337";
+// const localChainId = "31337";
 
 // const sleep = (ms) =>
 //   new Promise((r) =>
@@ -12,46 +12,95 @@ const localChainId = "31337";
 //     }, ms)
 //   );
 
-module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+// module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+//   const { deploy } = deployments;
+//   const { deployer } = await getNamedAccounts();
+//   const chainId = await getChainId();
+
+//   await deploy("Balloons", {
+//     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+//     from: deployer,
+//     // args: [ "Hello", ethers.utils.parseEther("1.5") ],
+//     log: true,
+//   });
+
+//   const balloons = await ethers.getContract("Balloons", deployer);
+
+//   await deploy("DEX", {
+//     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+//     from: deployer,
+//     args: [balloons.address],
+//     log: true,
+//     waitConfirmations: 5,
+//   });
+
+//   const dex = await ethers.getContract("DEX", deployer);
+
+//   // paste in your front-end address here to get 10 balloons on deploy:
+//   await balloons.transfer(
+//     "0x7f23ecdE95D4dB8fADD10B130ECC71141Da76ce7",
+//     "" + 10 * 10 ** 18
+//   );
+
+//   // // uncomment to init DEX on deploy:
+//   console.log(
+//     "Approving DEX (" + dex.address + ") to take Balloons from main account..."
+//   );
+//   // If you are going to the testnet make sure your deployer account has enough ETH
+//   // await balloons.approve(dex.address, ethers.utils.parseEther("100"));
+//   // console.log("INIT exchange...");
+//   // await dex.init("" + 3 * 10 ** 18, {
+//   //   value: ethers.utils.parseEther("0.1"),
+//   //   gasLimit: 200000,
+//   // });
+//   await balloons.approve(dex.address, ethers.utils.parseEther("100"));
+//   console.log("INIT exchange...");
+//   await dex.init("" + 3 * 10 ** 18, {
+//     value: ethers.utils.parseEther("0.1"),
+//     gasLimit: 200000,
+//   });
+// };
+// module.exports.tags = ["Balloons", "DEX"];
+
+// deploy/00_deploy_balloons_dex.js
+
+const { ethers } = require("hardhat");
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  const chainId = await getChainId();
-
   await deploy("Balloons", {
-    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
-    // args: [ "Hello", ethers.utils.parseEther("1.5") ],
     log: true,
   });
 
   const balloons = await ethers.getContract("Balloons", deployer);
 
   await deploy("DEX", {
-    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
     args: [balloons.address],
     log: true,
-    waitConfirmations: 5,
   });
 
   const dex = await ethers.getContract("DEX", deployer);
 
-  // paste in your front-end address here to get 10 balloons on deploy:
+  // paste in your address here to get 10 balloons on deploy:
   await balloons.transfer(
-    "0x08C01CEc8B8c793D768f502b604113074CE212aD",
+    "0x7f23ecdE95D4dB8fADD10B130ECC71141Da76ce7",
     "" + 10 * 10 ** 18
   );
 
-  // // uncomment to init DEX on deploy:
-  // console.log(
-  //   "Approving DEX (" + dex.address + ") to take Balloons from main account..."
-  // );
-  // // If you are going to the testnet make sure your deployer account has enough ETH
-  // await balloons.approve(dex.address, ethers.utils.parseEther("100"));
-  // console.log("INIT exchange...");
-  // await dex.init(ethers.utils.parseEther("5"), {
-  //   value: ethers.utils.parseEther("5"),
-  //   gasLimit: 200000,
-  // });
+  // uncomment to init DEX on deploy:
+  console.log(
+    "Approving DEX (" + dex.address + ") to take Balloons from main account..."
+  );
+
+  // If you are going to the testnet make sure your deployer account has enough ETH
+  await balloons.approve(dex.address, ethers.utils.parseEther("100"));
+  console.log("INIT exchange...");
+  await dex.init(ethers.utils.parseEther("0.1"), {
+    value: ethers.utils.parseEther("0.1"),
+    gasLimit: 200000,
+  });
 };
 module.exports.tags = ["Balloons", "DEX"];
